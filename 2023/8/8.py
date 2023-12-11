6920abe7452544e39fe67b29ce7d09c744d18296
Maximilian Wilhelm <max@sdn.clinic>
2023-12-11 22:47:05 +0100

    8: Part 2
    Signed-off-by: Maximilian Wilhelm <max@sdn.clinic>

--- 2023/8/8.py
+++ 2023/8/8.py
@@ -4,10 +4,11 @@
 #  --  Mon 11 Dec 2023 09:18:20 PM CET
 #
 
+from math import lcm
 import re
 import sys
 
-re_line = re.compile(r'([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)')
+re_line = re.compile(r'(\w+) = \((\w+), (\w+)\)')
 
 # LLR
 #
@@ -16,8 +17,9 @@ re_line = re.compile(r'([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)')
 # ZZZ = (ZZZ, ZZZ)
 
 pattern = []
+starters = []
 data_map = {
-    # [ <str>, <str> ],
+    # <str>: [ <str>, <str> ],
 }
 
 def parse_line(line: str) -> None:
@@ -28,12 +30,14 @@ def parse_line(line: str) -> None:
     if not match:
         raise Exception(f"Line didn't match: {line}")
     
-    data_map[match.group(1)] = [
-        match.group(2),
-        match.group(3),
-    ]
+    start, left, right = match.group(1), match.group(2), match.group(3)
 
-def trace_path(data_map) -> int:
+    data_map[start] = [ left, right ]
+
+    if start.endswith('A'):
+        starters.append(start)
+
+def trace_path_part1(data_map) -> int:
     """
     Trace the path from AAA to ZZZ and count the steps.
     """
@@ -53,6 +57,45 @@ def trace_path(data_map) -> int:
 
     return steps
 
+def trace_path_part2(data_map) -> int:
+    """
+    Trace the path from AAA to ZZZ and count the steps.
+    """
+    paths = [
+        # {
+        #   'node' : <str>,
+        # }
+    ]
+
+    # Set up paths
+    for start in starters:
+        paths.append({
+            'node': start,
+            'steps': 0,
+        })
+
+    for path in paths:
+        steps = 0
+        i = 0
+
+        while not path['node'].endswith('Z'):
+            index = 0 if pattern[i] == 'L' else 1
+            path['node'] = data_map[path['node']][index]
+
+            i = (i + 1) % len(pattern)    # Treat pattern list as ring buffer
+            steps +=1
+
+        path['steps'] = steps
+
+    return lcm(*[path['steps'] for path in paths])
+
+def all_paths_resolved(paths: list[dict]) -> bool:
+    for path in paths:
+        if not path['node'].endswith('Z'):
+            return False
+
+    return True
+
 if len (sys.argv) != 2:
 	print("Usage: 8.py file_path", file=sys.stderr)
 	sys.exit(1)
@@ -69,4 +112,5 @@ except IOError as e:
     print(f"File to open '{file_path}': {str(e)}", file=sys.stderr)
     sys.exit(1)
 
-print(f"Part 1: {trace_path(data_map)}")
+print(f"Part 1: {trace_path_part1(data_map)}")
+print(f"Part 2: {trace_path_part2(data_map)}")
